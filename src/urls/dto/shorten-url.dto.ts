b12df934419a0075c7bc3678759ecb2428be3316
Tien <tdtien7a1@gmail.com>
2026-04-02 01:54:54 +0700

    feat: add global validation pipe to check valid URL input

--- src/urls/dto/shorten-url.dto.ts
+++ src/urls/dto/shorten-url.dto.ts
@@ -1,5 +1,9 @@
 // src/urls/dto/shorten-url.dto.ts
 
+import { IsUrl, IsNotEmpty } from 'class-validator';
+
 export class ShortenUrlDto {
+  @IsNotEmpty({ message: 'Đường dẫn không được để trống!' })
+  @IsUrl({}, { message: 'Định dạng không hợp lệ. Phải là một URL (vd: https://google.com)' })
   originalUrl: string;
 }
--- src/urls/urls.controller.ts
+++ src/urls/urls.controller.ts
@@ -1,27 +1,23 @@
 // src/urls/urls.controller.ts
 import { Controller, Post, Body, Get, Param, Res } from '@nestjs/common';
-import type { Response } from 'express'; // <-- Chính là dòng cứu mạng này
+import type { Response } from 'express'; 
 import { UrlsService } from './urls.service';
 import { ShortenUrlDto } from './dto/shorten-url.dto';
 
-@Controller('urls') // (1) Định tuyến cha
+@Controller('urls') 
 export class UrlsController {
   
   // (2) Dependency Injection (Tiêm phụ thuộc)
   constructor(private readonly urlsService: UrlsService) {}
 
-  @Post('shorten') // (3) Định tuyến con
-  shorten(@Body() body: ShortenUrlDto) { // (4) Hứng dữ liệu
-    // Lễ tân nhận được link dài, ném sang cho phòng Service xử lý
+  @Post('shorten') 
+  shorten(@Body() body: ShortenUrlDto) { 
     return this.urlsService.shortenUrl(body.originalUrl);
   }
 
   @Get(':shortCode') 
   redirectUrl(@Param('shortCode') shortCode: string, @Res() res: Response) {
-    // 1. Nhờ Service tìm link gốc
     const originalUrl = this.urlsService.getOriginalUrl(shortCode);
-    
-    // 2. Ép trình duyệt chuyển hướng (Mã 302) sang link gốc
     return res.redirect(302, originalUrl);
   }
 
