d27409124c213ddc537f17175a12fa31b37357b7
Tien <tdtien7a1@gmail.com>
2026-04-04 02:16:43 +0700

    feat: configure postgres connection and register url repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'; 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UrlsModule } from './urls/urls.module';
+import { Url } from './urls/entities/url.entity'; 
 
 @Module({
-  imports: [UrlsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost', 
+      port: 5433, // Cổng của Docker
+      username: 'postgres', 
+      password: '123456', // Pass siêu bảo mật
+      database: 'postgres', // Dùng luôn database mặc định
+      entities: [Url], 
+      synchronize: true, 
+    }),
+    UrlsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/urls/urls.module.ts
+++ src/urls/urls.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UrlsService } from './urls.service';
 import { UrlsController } from './urls.controller';
+import { TypeOrmModule } from '@nestjs/typeorm'; 
+import { Url } from './entities/url.entity'; // Import Entity
 
 @Module({
+  // Cấp quyền cho module này xài bảng Url
+  imports: [TypeOrmModule.forFeature([Url])], 
+  controllers: [UrlsController],
   providers: [UrlsService],
-  controllers: [UrlsController]
 })
 export class UrlsModule {}
