50134c286a5df2ca64c4afcd2aabb4b1f7472d68
Tien <tdtien7a1@gmail.com>
2026-06-11 02:24:49 +0700

    test: update controller spec with getUrlStats mock

--- src/urls/urls.controller.spec.ts
+++ src/urls/urls.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UrlsController } from './urls.controller';
+import { UrlsService } from './urls.service';
 
 describe('UrlsController', () => {
   let controller: UrlsController;
@@ -7,6 +8,19 @@ describe('UrlsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UrlsController],
+      providers: [
+        {
+          provide: UrlsService,
+          useValue: {
+            shortenUrl: jest.fn(),
+            getOriginalUrl: jest.fn(),
+            getAllUrls: jest.fn(),
+            getUrlStats: jest.fn(),
+            updateUrl: jest.fn(),
+            deleteUrl: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UrlsController>(UrlsController);
