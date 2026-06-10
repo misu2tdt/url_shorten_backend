2f73c05fe1815ad23c559ddafc61b7c689f10607
Tien <tdtien7a1@gmail.com>
2026-06-11 02:24:48 +0700

    refactor: register global exception filter in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,21 @@
-// src/main.ts
-
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     }),
   );
+
+  app.useGlobalFilters(new GlobalExceptionFilter());
+
   app.enableCors();
 
   const config = app.get(ConfigService);
